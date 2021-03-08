c6d90b9fdba3a7e2970a9d3a094935bd6a5bcc14
Mark Lai <mlai468@gmail.com>
2021-03-08 12:28:03 -0700

    Modified for 346 NS

--- CalculateGain.m
+++ CalculateGain.m
@@ -1,22 +1,23 @@
 clear all;
 k = 1.38064852e-23;
+noiseBW = 800e3;
 tCold = 297;
-nsColdFilename = "C:\Users\Mark\OneDrive - University of Calgary\Thesis\Measurements\NS_Cal\NS-OFF.csv";
-nsHotFilename = "C:\Users\Mark\OneDrive - University of Calgary\Thesis\Measurements\NS_Cal\NS_ON.csv";
-nsENRFilename = "C:\Users\Mark\OneDrive - University of Calgary\Thesis\Measurements\NS_Cal\SNS_ENR.csv";
+nsColdFilename = "C:\Users\mlai4\OneDrive - University of Calgary\Thesis\Measurements\NS_Cal\New Cal\NS-OFF.csv";
+nsHotFilename = "C:\Users\mlai4\OneDrive - University of Calgary\Thesis\Measurements\NS_Cal\New Cal\NS_ON.csv";
+nsENRFilename = "C:\Users\mlai4\OneDrive - University of Calgary\Thesis\Measurements\Leo Custom ENR Tables\346C_ENRcor.ENR";
 coldDataCSV = readmatrix(nsColdFilename);
 coldData.freq = coldDataCSV(:,1);
-coldData.noiseDensity = coldDataCSV(:,5);
+coldData.noiseDensity = coldDataCSV(:,2);
 hotDataCSV = readmatrix(nsHotFilename);
 hotData.freq = hotDataCSV(:,1);
-hotData.noiseDensity = hotDataCSV(:,5);
-nsENR = readmatrix(nsENRFilename);
+hotData.noiseDensity = hotDataCSV(:,2);
+nsENR = readmatrix(nsENRFilename, 'FileType', 'text');
 
-interpolatedENR = interp1(nsENR(:,1)*1e6, nsENR(:,2), hotData.freq);
+interpolatedENR = interp1(nsENR(:,1)*1e9, nsENR(:,2), hotData.freq);
 
-hotNoiseDensityLinear = 10.^(hotData.noiseDensity/10)/1000;
-coldNoiseDensityLinear = 10.^(coldData.noiseDensity/10)/1000;
-gain = (hotNoiseDensityLinear - coldNoiseDensityLinear)./(k*tCold*(10.^(interpolatedENR/10)-1));
+hotNoiseDensityLinear = 10.^(hotData.noiseDensity/10)/1000 * noiseBW;
+coldNoiseDensityLinear = 10.^(coldData.noiseDensity/10)/1000 * noiseBW;
+gain = (hotNoiseDensityLinear - coldNoiseDensityLinear)./(k*tCold*(10.^(interpolatedENR/10)-1))/ noiseBW;
 
 gaindB = 10*log10(gain);
 
