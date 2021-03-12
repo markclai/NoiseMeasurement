258979131b8a11fdaf0b69ae67e0e37d3a125f73
Mark Lai <mlai468@gmail.com>
2021-03-11 19:20:32 -0700

    Updated measure to work with new files

--- measure.m
+++ measure.m
@@ -1,24 +1,29 @@
-pnaxAddress = 0;
-dmmAddress = 0;
-dcSupplyAddress = 0;
-impGenComPort = 0;
-gpibBoard = 0;
+pnaxAddress = "USB0::0x0957::0x0118::MY48420967::0::INSTR";
+dmmAddress = 22;
+dcSupplyAddress = 1;
+impGenComPort = 5;
+gpibBoard = 7;
 freqStart = 100e6;
 freqStop = 2e9;
 nPoints = 50;
-nAvg = 10;
+nAvg = 1;
 noiseBW = 800e3;
-noiseGain = "MED";
+noiseGain = "HIGH";
 pnaxCalSetName = "ML_Cal_Mar3";
 pnaxPortPower = -55;
 dmmIntTime = 100;
-dcVoltage = 56;
+dcVoltage = 6;
 dcCurrent = 100e-3;
 dutPowerChannel = 1;
 rfSwitch.channel1 = 2;
 rfSwitch.channel2 = 3;
 rfSwitch.voltage1 = 12;
 rfSwitch.voltage2 = 12;
+%% Make directory
+folderName = string(datetime('now'));
+folderName = erase(folderName, ":");
+mkdir(folderName);
+
 %% Connect to Lab instruments
 pnax = PNAX(pnaxAddress);
 dmm = DMM(dmmAddress, gpibBoard);
@@ -37,15 +42,25 @@ switchID = dcSupply.addSwitchSetup(rfSwitch.channel1, rfSwitch.channel2, rfSwitc
 for tunerState = 1:4
     dcSupply.setSwitchState(switchID, 1);
     tuner.setState(tunerState);
-    
-    pnax.saveS2P(sprintf("S2PState%d.csv", tunerState));
+    pause(5);
+    pnax.saveS2P(sprintf("./%s/S2PState%d.s2p", folderName, tunerState));
     dcSupply.setSwitchState(switchID, 2);
-    pnax.saveNoisePower(sprintf("NoisePower%d.csv", tunerState));
+    pause(5);
+    pnax.saveNoisePower(sprintf("./%s/NoisePower%d.csv", folderName, tunerState));
     tempKelvin(tunerState) = dmm.readTempKelvin();
 end
 
+%% Disable DC outputs
+for chanCounter = 1:4
+    dcSupply.disableOutput(chanCounter);
+end
+%% Disconnect all lab instruments
+pnax.checkStatusAndDisconnect();
+dmm.checkStatusAndDisconnect();
+dcSupply.checkStatusAndDisconnect();
+
 % Write tempertaure to csv file
-writematrix(tempKelvin, "RoomTempKelvin");
+csvwrite(sprintf("%s/RoomTempKelvin.csv", folderName), tempKelvin);
 
 
 
